0dcc9efcac77d1f5e67fe4266faceacd38774f98
Julian Affrime <julian.affrime@gmail.com>
2017-02-14 15:07:06 +0200

    added rating

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 
   var beers = [];
 
-  var addBeer = function(name, category) {
+  var addBeer = function(name, category, rating) {
     var newBeer = {
       "name": name,
-      "category": category
+      "category": category,
+      "rating": rating
     };
     beers.push(newBeer);
   };
@@ -14,14 +15,15 @@ $(document).ready(function(){
       $('.beers-list').empty();
 
       for (i=0; i<beers.length; i++) {
-        $('.beers-list').append("<p>" + beers[i].name + ": " + beers[i].category + "</p>");
+        $('.beers-list').append("<p>" + beers[i].name + " (" + beers[i].category + ") - Rating: " + beers[i].rating + "</p>");
       }
   };
 
   $('.post-beer').on('click', function(){
       var beerName = $('.beer-input').val();
       var beerCat = $('.category-input').val();
-      addBeer(beerName, beerCat);
+      var beerRating = $('.rating-input').val();
+      addBeer(beerName, beerCat, beerRating);
       updateBeers();
   });
 
