54dcdf80965c29701e3aa3bb2c8c5fa4c0bf25c1
Julian Affrime <julian.affrime@gmail.com>
2017-02-14 14:55:04 +0200

    part 3

--- script.js
+++ script.js
@@ -10,10 +10,19 @@ $(document).ready(function(){
     beers.push(newBeer);
   };
 
+  var updateBeers = function () {
+      $('.beers-list').empty();
+
+      for (i=0; i<beers.length; i++) {
+        $('.beers-list').append("<p>" + beers[i].name + ": " + beers[i].category + "</p>");
+      }
+  };
+
   $('.post-beer').on('click', function(){
       var beerName = $('.beer-input').val();
       var beerCat = $('.category-input').val();
       addBeer(beerName, beerCat);
+      updateBeers();
   });
 
   console.log(beers);
